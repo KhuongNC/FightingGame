99c1ffce44f46ce10e52c69082a186df82144f43
Khuongnc <anhkhuongmurom@gmail.com>
2022-08-22 14:55:30 +0700

    Health bar inteface

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100;
   }
 
   draw() {
@@ -161,7 +162,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
-    console.log("collision player");
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
   if (
@@ -169,7 +171,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
-    console.log("collision enemy");
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 }
 animate();
