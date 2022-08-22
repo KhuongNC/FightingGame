1650daeeeb163f0261436928a7eeb31a504d3cd7
Khuongnc <anhkhuongmurom@gmail.com>
2022-08-22 16:11:42 +0700

    Game timer and Game over

--- index.js
+++ index.js
@@ -131,6 +131,34 @@ function rectangleCollision({ rectangle1, rectangle2 }) {
   );
 }
 
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId);
+  document.querySelector("#displayText").style.display = "flex";
+  if (player.health == enemy.health) {
+    document.querySelector("#displayText").innerHTML = "TIE";
+  } else if (player.health > enemy.health) {
+    document.querySelector("#displayText").innerHTML = "PLAYER 1 WINS";
+  } else {
+    document.querySelector("#displayText").innerHTML = "PLAYER 2 WINS";
+  }
+}
+
+let timer = 11;
+let timerId;
+function decreaseTimer() {
+  timerId = setTimeout(decreaseTimer, 1000);
+  if (timer > 0) {
+    timer--;
+    document.querySelector("#timer").innerHTML = timer;
+  }
+
+  if (timer === 0) {
+    determineWinner({ player: player, enemy: enemy, timerId });
+  }
+}
+
+decreaseTimer();
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
@@ -174,6 +202,11 @@ function animate() {
     player.health -= 20;
     document.querySelector("#playerHealth").style.width = player.health + "%";
   }
+
+  // End game
+  if (enemy.health <= 0 || player.health <= 0) {
+    determineWinner({ player: player, enemy: enemy, timerId });
+  }
 }
 animate();
 
@@ -208,7 +241,7 @@ window.addEventListener("keydown", (event) => {
       enemy.velocity.y = -15;
       break;
     case "ArrowDown":
-      enemy.isAttacking = true;
+      enemy.attack();
       break;
   }
 });
