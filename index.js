7ec1d86da394669ed97d3c81b63c6f3a66774658
Khuongnc <anhkhuongmurom@gmail.com>
2022-08-23 08:22:00 +0700

    Background Sprite

--- index.js
+++ index.js
@@ -67,44 +67,6 @@ const keys = {
   },
 };
 
-function rectangleCollision({ rectangle1, rectangle2 }) {
-  return (
-    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
-      rectangle2.position.x &&
-    rectangle1.attackBox.position.x <=
-      rectangle2.position.x + rectangle2.width &&
-    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
-      rectangle2.position.y &&
-    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
-  );
-}
-
-function determineWinner({ player, enemy, timerId }) {
-  clearTimeout(timerId);
-  document.querySelector("#displayText").style.display = "flex";
-  if (player.health == enemy.health) {
-    document.querySelector("#displayText").innerHTML = "TIE";
-  } else if (player.health > enemy.health) {
-    document.querySelector("#displayText").innerHTML = "PLAYER 1 WINS";
-  } else {
-    document.querySelector("#displayText").innerHTML = "PLAYER 2 WINS";
-  }
-}
-
-let timer = 11;
-let timerId;
-function decreaseTimer() {
-  timerId = setTimeout(decreaseTimer, 1000);
-  if (timer > 0) {
-    timer--;
-    document.querySelector("#timer").innerHTML = timer;
-  }
-
-  if (timer === 0) {
-    determineWinner({ player: player, enemy: enemy, timerId });
-  }
-}
-
 decreaseTimer();
 
 function animate() {
